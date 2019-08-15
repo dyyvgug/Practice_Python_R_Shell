d1f581f66e2ae0b88073ac289e87c189da4730b8
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2019-08-15 11:14:02 +0800

    Update sliding_window.py

--- bioinformation/sliding_window.py
+++ bioinformation/sliding_window.py
@@ -1,5 +1,5 @@
 # sliding window
-# given window is 5 base,step size of 1 base.
+# given window is 5 amino acid,step size of 1 amino acid.
 seq = "PRQTEINSEQWENCE"
 for i in range(len(seq)-4):
 	print (seq[i:i+5])
