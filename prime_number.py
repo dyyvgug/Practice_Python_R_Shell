1828e780d07b4939be2f3c0273fc59de39010266
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2019-08-19 17:13:25 +0800

    Add files via upload

---
+++ prime_number.py
@@ -0,0 +1,12 @@
+for i in range(30):
+	if i < 4 :
+		print ("prime number:",i)
+	elif i % 2 == 0:
+		print ("multiple of two:",i)
+	elif i % 3 == 0 :
+		print ("multiple of three:",i)
+	elif i % 5 == 0 :
+		print ("multiple of five:",i)
+	else:
+		print ("prime number:",i)
+
