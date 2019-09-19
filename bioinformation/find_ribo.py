5182bb417b9ff65821cf75b5dc6b6485fafc0bcc
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2019-09-19 21:06:46 +0800

    Update find_ribo.py

--- bioinformation/find_ribo.py
+++ bioinformation/find_ribo.py
@@ -6,10 +6,10 @@ import re
 os.chdir("/home/hp/Desktop/other_riboseq/C_elegans_Ensl_WBcel235/experiment2/aligned/ribo_num/")
 other_ribo = open("SRR1804340_other_ribo_gene.txt",'r')
 hE_hT = open("SRR1804340_hiE_ht_gene.txt",'r')
-other_ribo_filter = open("40_other_ribo_filter.txt",'a')
-hE_hT_filter = open("40_hE_ht__ribo_filter.txt",'a')
-#keyword1 = re.compile('rpl')
-keyword2 = re.compile('rps')
+other_ribo_filter = open("40_other_ribo_filter.txt",'w')
+hE_hT_filter = open("40_hE_ht__ribo_filter.txt",'w')
+hE_hT_no_ribo = open("40_hE_ht-ribo.txt",'w')
+keyword = re.compile(r'rpl|rps')
 def findRibo(ribokey):
 	for line in other_ribo:
 		riboLine = ribokey.search(line)
@@ -19,8 +19,12 @@ def findRibo(ribokey):
 		riboLine = ribokey.search(line)
 		if riboLine:
 			hE_hT_filter.write(line)
+		else:
+			hE_hT_no_ribo.write(line)
 #findRibo(keyword1)
-findRibo(keyword2)
+findRibo(keyword)
 other_ribo_filter.close()
 hE_hT_filter.close()
+hE_hT_no_ribo.close()
+
 		
