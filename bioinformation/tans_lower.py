039f8c3fa102b11db048d86ff06e55b1cdd81c20
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2019-09-19 19:29:10 +0800

    Update tans_lower.py

--- bioinformation/tans_lower.py
+++ bioinformation/tans_lower.py
@@ -8,14 +8,3 @@ def getText():
     for ch in '!@#$%^&*[]\{}\|?/><_+-=()~`':
         txt = txt.replace(ch," ")
     return txt
-
-hamletTxt = getText()
-words = hamletTxt.split()
-counts = {}
-for word in words:
-    counts[word] = counts.get(word,0) + 1
-items = list(counts.items())
-items.sort(key=lambda x:x[1],reverse=True)
-for i in range(11):
-    word,count = items[i]
-    print("{0:<10}{1:>5}".format(word,count))
