90f0fba3a0a7d981162384c95c268f26f485c05b
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2019-09-19 19:31:31 +0800

    Update tans_lower.py

--- bioinformation/tans_lower.py
+++ bioinformation/tans_lower.py
@@ -1,10 +1,8 @@
 #tran_lower.py
-def getText():
-    fasta = [ATXN1.fa,DDX5.fa,DHX9.fa,EWSR1.fa,FUS.fa,HnRNPA1.fa,TAF15.fa,TDP43.fa,TIA1.fa]
-    fasta = "".join.fasta
-    for i in fasta:
-        txt = open(".fa","r").read()
-        txt = txt.lower()
-    for ch in '!@#$%^&*[]\{}\|?/><_+-=()~`':
-        txt = txt.replace(ch," ")
-    return txt
+txt = open("FUS.fa","r").read()
+txt = txt.upper()
+
+with open('FUS.fa', 'w') as ff:
+    ff.writelines(txt)
+
+ff.close()
