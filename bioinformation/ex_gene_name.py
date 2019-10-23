1111a520b6b11c2cf0637a30fa8dd0e1dbd3535e
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2019-10-23 09:30:22 +0800

    Create ex_gene_name.py

---
+++ bioinformation/ex_gene_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#coding:utf-8
+#2019-10-23.Yingying Done.
+
+fa_file = open('transcripts.fa','r')
+name_file = open('gene_name.txt','w')
+
+for line in fa_file:
+	if line.startswith('>'):    
+# The sequence title is now obtained.
+		header = line
+		print (header)
+		name_file.write(header + '\n')
+fa_file.close()
+name_file.close()
