c2e7539008cb539485b8d7917b082bc1da5edb44
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2020-01-07 11:22:11 +0800

    Update find_seq.py

--- bioinformation/find_seq.py
+++ bioinformation/find_seq.py
@@ -1,36 +1,39 @@
 #!/usr/bin/python
 #coding:utf-8
 #=========================================================================================================
-# Yingying Dong.2019-10-30.Get gene sequence of high expression high translation from CDS sequence.
+# Yingying Dong.2019-10-30.Modified date:2020-1-7.Get gene sequence of high expression high translation from CDS sequence.
 #=========================================================================================================
 import os
 import argparse
 
 parser = argparse.ArgumentParser(description='find DNA sequence',prog='find seq',usage='%(prog)s [options]')
 parser.add_argument('--spe', nargs='?',type=str,help='species name')
-parser.add_argument('--exp',nargs='?', type=int,help='experiment number')
+parser.add_argument('--samp', nargs='?',type=str,help='sample name')
+parser.add_argument('--exp', nargs='?',type=str,help='experiment lab number')
+parser.add_argument('--per', nargs='?',type=str,help='expression level')
 args = parser.parse_args()
 
 
 os.chdir('/media/hp/disk1/DYY/reference/annotation/{}/ref'.format(args.spe))
 DNA = open('CDS_DNA.fa','r')
-hE_hT_path = '/home/hp/Desktop/other_riboseq/{}/experiment{}/aligned/ribo_num/'.format(args.spe,args.exp)
-hE_hT = open('{}hE_hT_only_geneID.txt'.format(hE_hT_path),'r')
-hE_hT_seq = open('{}hE_hT_seq.fa'.format(hE_hT_path),'w')
+hE_path = '/media/hp/Katniss/DYY/aligned/{}/experiment{}/'.format(args.spe,args.exp)
+hE = open('{}{}_{}_exp_only_name.txt'.format(hE_path,args.samp,args.per),'r')
+hE_seq = open('{}{}_{}_seq.fa'.format(hE_path,args.samp,args.per),'w')
 
-hE_hT_table = []
+hE_table = []
 database = {}
-for i in hE_hT.readlines():
-    hE_hT_table.append(i.strip())
+for i in hE.readlines():
+    hE_table.append(i.strip())
 for j in DNA:
     if j.startswith('>'):
         keys = j.lstrip('>').strip()
         database[keys] = []
     else:
         database[keys].append(j.strip())
-for line in hE_hT_table:
-    hE_hT_seq.write('>' + str(line) + '\n' + str(''.join(database[line])) + '\n')
+for line in hE_table:
+    if line in database.keys():
+        hE_seq.write('>' + str(line) + '\n' + str(''.join(database[line])) + '\n')
 
-hE_hT.close()
+hE.close()
 DNA.close()
-hE_hT_seq.close()
+hE_seq.close()
