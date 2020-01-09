c3d6027231ac180baa69bcb32866e7a50083556b
Yingying Dong <37611412+KatnissD@users.noreply.github.com>
2020-01-09 10:25:58 +0800

    Update replace_cha_at_spe_position.py

--- bioinformation/replace_cha_at_spe_position.py
+++ bioinformation/replace_cha_at_spe_position.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python
 #coding:utf-8
+#Yingying Dong.
 import re
 p = re.compile('A{5,}')
-string = 'ATGCAGAAAAATGCTACCGAAAAAACGGGAAAAACCC'
+string = 'ATGCAGAAAAATGCTACCGAAAAAAAAAAAAAAACGGGAAAAACCC'
 # string = 'ATGCAGUUAATGCTACCXXXAAAACGGGAQQQACCC'
 pos_list = []
-for m in p.finditer(string):
-	pos = m.start()
-	pos_list.append(pos)
-print(pos_list)
-for i in pos_list:
-	if i % 3 == 0:
-		print(string[i:i+3])
-		string = string[:i] + 'UUU' + string[i+3:]
-		print(string)
-	elif i % 3 == 1:
-		print(string[i -1 : i + 2])
-		string = string[:i-1] + 'XXX' + string[i+2:]
-		print(string)
-	elif pos % 3 == 2:
-		print(string[i + 1 : i + 4])
-		string = string[:i+1] + 'QQQ' + string[i+4:]
-		print(string)
+while len(p.findall(string)):
+	for m in p.finditer(string):
+		pos = m.start()
+		pos_list.append(pos)
+	print(pos_list)
+	for i in pos_list:
+		if i % 3 == 0:
+			print(string[i:i+3])
+			string = string[:i] + 'UUU' + string[i+3:]
+			print(string)
+		elif i % 3 == 1:
+			print(string[i -1 : i + 2])
+			string = string[:i-1] + 'XXX' + string[i+2:]
+			print(string)
+		elif pos % 3 == 2:
+			print(string[i + 1 : i + 4])
+			string = string[:i+1] + 'QQQ' + string[i+4:]
+			print(string)
 print(string)
 
+
+	
