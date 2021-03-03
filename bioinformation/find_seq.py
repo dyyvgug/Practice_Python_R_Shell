e4fc95dcd667a00315e71fb21762d0efa177a2a3
KatnissD <dyyvgug@163.com>
2021-03-03 15:01:50 +0800

    recover

--- bioinformation/find_seq.py
+++ bioinformation/find_seq.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python
 #coding:utf-8
 #=========================================================================================================
-# Yingying Dong.2019-10-30.Modified date:2021-2-28.
-# Get gene sequences of top 1% TPM from CDS sequence.
+# Yingying Dong.2019-10-30.Modified date:2020-1-7.Get gene sequence of high expression high translation from CDS sequence.
 #=========================================================================================================
 import os
+import argparse
 
-print(os.getcwd())
+parser = argparse.ArgumentParser(description='find DNA sequence',prog='find seq',usage='%(prog)s [options]')
+parser.add_argument('--spe', nargs='?',type=str,help='species name')
+parser.add_argument('--samp', nargs='?',type=str,help='sample name')
+parser.add_argument('--exp', nargs='?',type=str,help='experiment lab number')
+parser.add_argument('--per', nargs='?',type=str,help='expression level')
+args = parser.parse_args()
 
-#os.chdir('/media/hp/disk1/DYY/reference/annotation/{}/ref'.format(args.spe))
-'''DNA = open('CDS_DNA.fa', 'r')
-hE = open('{}.txt'.format(hE_path), 'r')
+
+os.chdir('/media/hp/disk1/DYY/reference/annotation/{}/ref'.format(args.spe))
+DNA = open('CDS_DNA.fa','r')
+hE_path = '/home/hp/Desktop/other_riboseq/{}/experiment{}/aligned/ribo_num/'.format(args.spe,args.exp)
+hE = open('{}{}_{}_only_geneID.txt'.format(hE_path,args.samp,args.per),'r')
 hE_seq = open('{}{}_{}_seq.fa'.format(hE_path,args.samp,args.per),'w')
 
 hE_table = []
@@ -29,4 +36,4 @@ for line in hE_table:
 
 hE.close()
 DNA.close()
-hE_seq.close()'''
+hE_seq.close()
