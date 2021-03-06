addf819dae63342e35e957bfcd212449ab110707
KatnissD <dyyvgug@163.com>
2021-03-06 16:32:29 +0800

    modification

--- learn_os.py
+++ learn_os.py
@@ -7,12 +7,12 @@ print(os.getcwd())              # get current path
 print(os.listdir('.'))          # get all files of current path (list)
 print(os.listdir(os.getcwd()))  # quality (list)
 
-items = os.listdir(os.getcwd())     # one by one
+items = os.listdir(os.getcwd())     # one by one (str)
 for item in items:
     if os.path.isfile(item):
         print(item)
 
-print(os.walk('.'))                  # another way
+print(os.walk('.'))                  # another way (str)
 for root, dirs, files in os.walk('.'):
     for name in files:
         print(os.path.join(root, name))
@@ -28,6 +28,7 @@ syst = platform.system()            # transform path
 if os.path.exists('{}{}'.format(the_path, "hello.txt")):
     c = open('{}{}'.format(the_path, "hello.txt"), 'r')
 
-
+if not os.path.exists('./d/'):
+    os.mkdir("./d/")
 
 
