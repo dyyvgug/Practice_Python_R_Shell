d14fe52637b8bf6635529f355e2564967fc1b106
KatnissD <dyyvgug@163.com>
2021-09-07 11:18:15 +0200

    modify

--- Shell/loop_array.sh
+++ Shell/loop_array.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #learning loop array in Shell
 
-numbers=('32234' '5883492' '43984' '12454' '84732')
+numbers=('32234' '5883492' '43984' '12454' '84732' '63282')
 for item in ${numbers[@]}
 do
 echo ${item}
@@ -13,5 +13,6 @@ i=0
 until [ ! $i -lt 5 ]
 do
 echo "position $i is ${numbers[$i]} "
+echo "next position `expr $i + 1` is ${numbers[`expr $i + 1`]}"
 i=`expr $i + 1`
 done
