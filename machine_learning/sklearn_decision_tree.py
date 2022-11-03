5cd219714d20f8820e264d5c37010fcb86569268
Yingying Dong <37611412+dyyvgug@users.noreply.github.com>
2022-11-03 16:11:51 +0100

    Update sklearn_decision_tree.py

--- machine_learning/sklearn_decision_tree.py
+++ machine_learning/sklearn_decision_tree.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import graphviz
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
-from sklearn.model_selection import train_test_split, KFold
+from sklearn.model_selection import train_test_split, KFold, GridSearchCV
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, \
     precision_recall_fscore_support, roc_auc_score, roc_curve
 from IPython.display import Image
@@ -55,3 +55,18 @@ dt2.fit(x, y)
 dot_file = export_graphviz(model, feature_names=feature_names)
 graph = graphviz.Source(dot_file)  # dot to img
 graph.render(filename='decision_tree', format='png', cleanup=True)
+
+# ----------------------- Pruning ---------------------------
+# Pruning the decision tree to fix the overfitting
+dt3 = DecisionTreeClassifier(max_depth=3, min_samples_leaf=2, max_leaf_nodes=10)
+# Sklearn has Grid search class to do loop cross validation, then we can decide how pruning
+param_grid = {
+    'max_depth': [5, 15, 25],
+    'min_samples_leaf': [1,3],
+    'max_leaf_nodes': [10,20,35,50]
+}
+dt4 = DecisionTreeClassifier()
+gs = GridSearchCV(dt4, param_grid, scoring='f1', cv=5)  # 5-fold cross validation
+gs.fit(X,y)
+print('best params:', gs.best_params_)
+print('best score:', gs.best_score_)
