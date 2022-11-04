191cbd7a8f93a62c01e1fe81dbc14502b97cb434
Yingying Dong <37611412+dyyvgug@users.noreply.github.com>
2022-11-04 16:08:01 +0100

    Add files via upload

---
+++ machine_learning/sklearn_RF.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.datasets import load_breast_cancer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+cancer_data = load_breast_cancer()
+print(cancer_data['data'].shape) # or print(cancer_data.data.shape)
+
+df = pd.DataFrame(cancer_data['data'], columns=cancer_data['feature_names'])
+df['target'] = cancer_data['target']  # 0 means malignant and 1 means benign
+#print(df.head())
+#print(cancer_data.feature_names)
+#print(df[cancer_data.feature_names])
+X = df[cancer_data.feature_names].values
+y = df['target'].values
+
+x_train, x_test, y_train, y_test = train_test_split(X,y,random_state=101)
+rf = RandomForestClassifier()
+rf.fit(x_train, y_train)
+
+first_row = x_test[0]
+print("prediction:", rf.predict([first_row]))
+print("true value:", y_test[0])
