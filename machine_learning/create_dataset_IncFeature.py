0bed15a971ff9cabe8a8746f88d99bf3b7fa2643
Yingying Dong <37611412+dyyvgug@users.noreply.github.com>
2023-01-15 11:30:33 +0800

    Update create_dataset_IncFeature.py

--- machine_learning/create_dataset_IncFeature.py
+++ machine_learning/create_dataset_IncFeature.py
@@ -1,4 +1,6 @@
 from sklearn.datasets import make_classification
+from matplotlib import pyplot as plt
+
 X, y = make_classification(n_features=2, n_redundant=0, n_informative=2, random_state=3)
 print(X)
 print(y)
@@ -8,3 +10,7 @@ print(y)
 i,j = make_classification (n_features =3, n_redundant =0, random_state =5, n_informative =2)
 print (i)
 print (j)
+
+plt.scatter(X[y==0][:, 0], X[y==0][:, 1], s=100, edgecolors='k')
+plt.scatter(X[y==1][:, 0], X[y==1][:, 1], s=100, edgecolors='k', marker='^')
+plt.show()
