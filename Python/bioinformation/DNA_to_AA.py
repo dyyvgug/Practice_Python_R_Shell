6620df8d7386101ef6e89aad54e63a3bf885929b
Yingying Dong <37611412+dyyvgug@users.noreply.github.com>
2023-07-12 12:30:24 +0200

    Update DNA_to_AA.py

--- Python/bioinformation/DNA_to_AA.py
+++ Python/bioinformation/DNA_to_AA.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python
 #coding:utf-8
-#2019-8-22.Yingying Done.
+# 2019-8-22.Yingying Dong.
+import argparse
+
+parser = argparse.ArgumentParser(description='DNA to AA.', prog='dna aa', usage='%(prog)s [options]')
+parser.add_argument('-inp', nargs='?', required=True, type=str, help='The FASTA file of DNA sequences')
+parser.add_argument('-o', nargs='?', type=str, default='amino_acid.fa',
+                    help='The file name of output amino acid sequences.The default file name is \'amino_acid.fa\'')
+args = parser.parse_args()
+
 codon_table = {'GCT':'A','GCC':'A','GCA':'A','GCG':'A','CGT':'R','CGC':'R','CGA':'R',\
 	'CGG':'R','ACT':'T','ACC':'T','ACA':'T','ACG':'T','GTT':'V','GTC':'V','GTA':'V',\
 	'GTG':'V','GGT':'G','GGC':'G','GGA':'G','GGG':'G','TCT':'S','TCC':'S','TCA':'S',\
@@ -11,13 +19,13 @@ codon_table = {'GCT':'A','GCC':'A','GCA':'A','GCG':'A','CGT':'R','CGC':'R','CGA'
 	'TTG':'L','TGT':'C','TGC':'C','TGG':'W','TAT':'Y','TAC':'Y','TAA':'STOP','TAG':'STOP',\
 	'TGA':'STOP'}
 
-with open('GFP_Kp.fa', 'r+') as f:
+with open(args.inp, 'r+') as f:
 	f.seek(0, 2)
 	f.write('>')
 f.close()
 
-dna_file = open('GFP_Kp.fa','r')
-aa_file = open('GFP_Kp_aa.txt','w')
+dna_file = open(args.inp,'r')
+aa_file = open(args.o,'w')
 
 dna = ''
 for line in dna_file:
