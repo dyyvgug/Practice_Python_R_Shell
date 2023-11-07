05f4476b345cbdaa2eb700d66648f2ee99cdf7ca
Yingying Dong <37611412+dyyvgug@users.noreply.github.com>
2023-11-07 12:34:12 +0100

    Create create_jupyter_notebook.sh

---
+++ Python/basics/create_jupyter_notebook.sh
@@ -0,0 +1,11 @@
+# creating an isolated environment(recommended)
+
+    $ python -m pip install -U virtualenv
+    $ python -m virtualenv my_env
+    $ source my_env/bin/activate
+
+# deactivate can close the environment
+
+    $ python -m  pip install -U jupyter matplotlib numpy pandas scikit-learn
+    $ python -m ipykernel install --name=my_env
+    $ jupyter notebook
